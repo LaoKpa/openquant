1eccabd052b54b20c964d9024f4c66a837d03cd5
Sean Koval <sean@capitalprawn.com>
2020-11-02 23:50:14 -0500

    bb_add

---
+++ src/Strategies/rsi_simple.py
@@ -0,0 +1,46 @@
+def relative_strength_index(df, n):
+        """Calculate Relative Strength Index(RSI) for given data.
+        https://github.com/Crypto-toolbox/pandas-technical-indicators/blob/master/technical_indicators.py
+
+        :param df: pandas.DataFrame
+        :param n:
+        :return: pandas.DataFrame
+        """
+        i = 0
+        UpI = [0]
+        DoI = [0]
+        while i + 1 <= df.index[-1]:
+            UpMove = df.loc[i + 1, 'high'] - df.loc[i, 'high']
+            DoMove = df.loc[i, 'low'] - df.loc[i + 1, 'low']
+            if UpMove > DoMove and UpMove > 0:
+                UpD = UpMove
+            else:
+                UpD = 0
+            UpI.append(UpD)
+            if DoMove > UpMove and DoMove > 0:
+                DoD = DoMove
+            else:
+                DoD = 0
+            DoI.append(DoD)
+            i = i + 1
+        UpI = pd.Series(UpI)
+        DoI = pd.Series(DoI)
+        PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean())
+        NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean())
+        RSI = pd.Series(round(PosDI * 100. / (PosDI + NegDI)), name='RSI_' + str(n))
+        # df = df.join(RSI)
+        return RSI
+
+def get_rsi(data, window=14):
+    """
+    This function pulls the RSI of a given widow.
+
+    :param data: (pd.DataFrame) price returns
+    :param window: set window for the scope of calculation
+    :return: (pd.DataFrame) RSI values over the window
+    """
+    df = data.copy(deep=True).reset_index()
+    rsi = relative_strength_index(df, window)
+    rsi_df = pd.Series(data=rsi.values, index=data.index)
+
+    return rsi_df
